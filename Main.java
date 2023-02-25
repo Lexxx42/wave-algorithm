e6222d604cb92aec9dd614a984b01c181ae65be3
Alexander Konukhov <jesusmisha@gmail.com>
2023-02-25 15:13:15 +0300

    Generation and Printing done

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
-import GenerateTable;
-
 public class Main {
     public static void main(String[] args) {
-
+        int[][] table = GenerateTable.getMap();
+        String tableForPrint = PrintTable.getMap(table);
+        PrintTable.printArray(tableForPrint);
     }
 }
--- PrintTable.java
+++ PrintTable.java
@@ -1,2 +1,23 @@
 public class PrintTable {
+    private PrintTable() {
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static String getMap(int[][] map) {
+        //return Arrays.deepToString(map);
+        StringBuilder result = new StringBuilder();
+        result.append('\n');
+        for (int[] arr : map) {
+            for (int element : arr) {
+                result.append('\t');
+                result.append(element);
+            }
+            result.append('\n');
+        }
+        return result.toString();
+    }
+
+    public static void printArray(String array) {
+        MyLogger.writeLog(array);
+    }
 }
