104630b310936954a1bff1b1333b195a9c91fb46
Anton Sizykh <toxa-v-s@yandex.ru>
2023-02-25 21:14:32 +0300

    added gradient blocks for filled maps. toDO - drawing path lines

--- printPretty.java
+++ printPretty.java
@@ -1,4 +1,7 @@
 import java.awt.*;
+import java.util.IntSummaryStatistics;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
 public class printPretty {
 
@@ -18,6 +21,8 @@ public class printPretty {
 
     public static String printBlockMap(int[][] arr){
         StringBuilder blocs_map_str = new StringBuilder(arr.length);
+        int gradient_max = find2DMaxStream(arr);
+        //int gradient_max = find2DMaxLoop(arr); // немного медленнее
 
         for (int[] row:arr){
             for (int cell: row){
@@ -26,20 +31,25 @@ public class printPretty {
                         blocs_map_str.append(Character.toString(0x2588));
                         break;
                     case 1:
-                        blocs_map_str.append(Character.toString(0x25E2)); //00d7 tiny  cross ,25C9 - bullseye, slightly wide
+                        blocs_map_str.append(Character.toString(0x25B2)); //00d7 tiny  cross ,25C9 - bullseye, slightly wide, 25E2 - лесенка
                         break;
                     case 0:
                         blocs_map_str.append(" ");
                         break;
                     case -2:
                         blocs_map_str.append(Character.toString(0x25BC)); // значок прицела 2316
+                        break;
                     default:
+                        if(cell>gradient_max*2/3)
+                            blocs_map_str.append(Character.toString(0x2593));
+                        else if(cell>gradient_max/3)
+                            blocs_map_str.append(Character.toString(0x2592));
+                        else
+                            blocs_map_str.append(Character.toString(0x2591));
                         break;
                 }
             }
             blocs_map_str.append("\n");
-            // System.out.println(Arrays.toString(row));
-            // System.out.println(blocs_map_str);
         }
 
         return blocs_map_str.toString();
@@ -47,5 +57,22 @@ public class printPretty {
 
     public static void showPath(int[][] arr, String path_str){
 
+    }
+ // статистические встроенные методы полсе JAVA 8. Работают со Stream
+    public static int find2DMaxStream(int[][] arr){
+        IntSummaryStatistics stats = Stream.of(arr)
+                .flatMapToInt(IntStream::of)
+                .summaryStatistics();
+        return stats.getMax();
+    }
+
+    public static int find2DMaxLoop(int[][] arr){
+        int min = Integer.MIN_VALUE;
+        for (int[] row: arr){
+            for (int cell: row){
+                if (cell>min) { min=cell;}
+            }
+        }
+        return min;
     }
 }
