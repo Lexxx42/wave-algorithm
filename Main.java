c7a90ea201ce9ae9480dce6fb7d88a4721d863b3
Alexander Konukhov <jesusmisha@gmail.com>
2023-02-26 11:45:42 +0300

    class print

--- Main.java
+++ Main.java
@@ -1,16 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        int[][] table = GenerateTable.getMap();
-        String tableForPrint = PrintTable.getMap(table);
+        MazeMap maze = new MazeMap();
+        String tableForPrint = PrintTable.getMap(maze.map);
         PrintTable.printMap(tableForPrint);
-
-        WaveAlgorithm.colorize(table);
-        tableForPrint = PrintTable.getMap(table);
-        PrintTable.printMap(tableForPrint);
-
-        System.out.println(PrintPretty.printBlockMap(table));
-        String testPath = "1:4,1:3,2:3,3:3,4:3,4:2,4:1,3:1,2:1,1:1";
-        int[][] testMaze = PrintPretty.showPath(table, testPath);
-        System.out.println(PrintPretty.printBlockMap(testMaze));
     }
 }
