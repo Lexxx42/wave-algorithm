3db48fbeae84a786c2d2ba0accea0a0af2659a44
Alexandr Konukhov <jesusmisha@gmail.com>
2023-03-01 08:48:49 +0300

    deleted comments from main

--- Main.java
+++ Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        //int[][] table = GenerateTable.getMap();  // Static Map
         int[][] table = MapGenerator.generateMaze(13, 13, true, 1);  // Generated Map
         String tableForPrint = PrintTable.getMap(table);
         PrintTable.printMap(tableForPrint);
@@ -10,15 +9,10 @@ public class Main {
         PrintTable.printMap(tableForPrint);
 
         System.out.println(PrintPretty.printBlockMap(table));
-        //String a = "1:4,1:3,2:3,3:3,4:3,4:2,4:1,3:1,2:1,1:1";
         String testPath = SearchWay.SearchWays(table);
-        //System.out.println(a);
         System.out.println(testPath);
-
 
         int[][] testMaze1 = PrintPretty.showPath(table, testPath);
         System.out.println(PrintPretty.printBlockMap(testMaze1));
-        //int[][] testMaze2 = PrintPretty.showPath(table, a);
-        //System.out.println(PrintPretty.printBlockMap(testMaze2));
     }
 }
